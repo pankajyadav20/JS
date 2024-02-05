24e950468613cb28489cc921881eab69ff3fcf55
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-05 14:50:35 +0000

    container config do

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId="22222";
+let accountemail="pk@gmail.com"
+var accountpassword="123456"
+accountcity="kanpur"
+let accountstate;
+//accountId="333"
+accountemail ="ak@gmail.com"
+accountpassword="22244"
+accountcity="locknow"
+console.log(accountId);
+console.table([accountId,accountemail, accountpassword,accountcity,accountstate])
