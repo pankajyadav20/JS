26a3856ce540a4510b3b40dfd7c4a4f74ef1f0d8
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-06 17:15:49 +0000

    conversion of datatype

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,2 +1,9 @@
 "use strict" //treat all js code as new version
- 
+ //type of datatypes
+ //number ==>2 to power 53
+ // string="" or ''
+ //boolean=true/false
+ //null=standalone value
+ //undefined =>value not asign
+// symbol =>unique
+console.log(typeof null);//result after run => object
---
+++ 01_basics/02_datatypeconversion.js
@@ -0,0 +1,22 @@
+//let score= "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//if score=>null then value is 0
+//if score=>undefined then value is NAN(not a number)
+/*let ram ="true"
+let booleanram=Boolean
+(ram) 
+console.log(booleanram);*/
+//if take 1=>true
+//0=>false
+//""=>false
+// "string"=>true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); 
