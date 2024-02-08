41d2d7a546ed06ddf7b5b3ba0618efa5346d3347
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-08 13:07:27 +0000

    math and num

---
+++ 01_basics/05_nummath.js
@@ -0,0 +1,14 @@
+const othernumber =123.8966
+//console.log(othernumber.toPrecision(4));
+const hundreds = 1000000
+//console.log(hundreds);
+//console.log(hundreds.toLocaleString('en-IN'));
+/*........****maths******..*/
+//  console.log(Math);
+//  console.log(Math.abs(-4));
+//  console.log(Math.floor(4.4));
+//  console.log(Math.floor(4.6));
+//  console.log(Math.floor(4.9));
+console.log(Math.random
+    ());
+    console.log(Math.random()*10);
