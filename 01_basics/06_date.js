e66ad5ade5f3b2d4f0396749cb0f7d3feabfe12a
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-08 14:27:27 +0000

    date and time

---
+++ 01_basics/06_date.js
@@ -0,0 +1,6 @@
+const myDate = new Date()
+console.log(myDate.toString());
+console.log(typeof myDate);
+//let date= new Date(2024, 12,8)
+let date= new Date("13-13-2024")
+console.log(date.toLocaleString());
