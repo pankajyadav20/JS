8a4d6470de5cb76076a899a786cf3e857d83a278
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-22 10:04:11 +0000

    scope and arrow

---
+++ 02_basics/04_scope.js
@@ -0,0 +1,20 @@
+// closer --> andar ka functuon bahar ke variable ko access kar sakta hai 
+if(true){
+    const username ="pankaj"
+    if(username=== "pankaj"){
+        const website = " youtube"
+        //console.log( username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+// *******************
+//two type to write function
+addone(5);
+function addone(num){
+    return num +1;
+}
+addtwo(5);
+const addtwo =function(num){
+    return num +2;
+}
---
+++ 02_basics/05_arrow.js
@@ -0,0 +1,8 @@
+// arrow function
+// const addtwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(addtwo(4,5));
+const addtwo = (num1, num2) => num1 + num2
+
+console.log(addtwo(4,5));
