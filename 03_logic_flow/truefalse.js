19d069dbdcb1b28e17a6e8b26c27ffa4d284c4d4
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-02-22 11:33:39 +0000

    loop

---
+++ 03_logic_flow/truefalse.js
@@ -0,0 +1,25 @@
+const userEmail =[]
+// if(userEmail){
+//     console.log("i got email");
+// }
+// else{
+//     console.log("don't have email" );
+// }
+
+
+//falsy value
+/*   false, 0 ,-0, BigInt 0n, "", null, undefined,NaN            */
+//truthy value
+/*    "0", 'false', "  ", [], {},function(){}          */ 
+if (userEmail.length=== 0){
+    //console.log("array is empty");
+}
+const emptyobj =[]
+if(Object.keys(emptyobj).length === 0){
+    console.log("object is empty");
+}else {
+    console.log("no empty");
+}
+/////////////
+// terniary operators
+//  condition ? true : false
---
+++ loops/one.js
@@ -0,0 +1,22 @@
+// for (let i = 1; i <10; i++) {
+//     const element =i;
+//     console.log(element);
+    
+// }
+// for (let index = 0; index < 15; index++) {
+//     if(index==5){
+//         console.log('detected 5');
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+for (let index = 0; index < 15; index++) {
+    if(index==5){
+        console.log('detected 5');
+        continue;
+    }
+    console.log(`value of i is ${index}`);
+    
+}
